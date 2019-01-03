33010c96e5f9f8c9cf3ded1dc39742d6ad5cf4a7
Zhengyao Xue <zhxue@ucdavis.edu>
2019-01-02 17:23:22 -0800

    Modify function to plot CFU over time

--- cheese_inoculation.R
+++ cheese_inoculation.R
@@ -6,38 +6,40 @@ path <- "G:/My Drive/UC_Davis/Marco_lab/milk_microbiota/2017_Hilmar_sampling/iso
 
 tab <- read.csv(file.path(path,"sample information sheet.csv"))
 
-tab.milk <- subset(tab, Inoculumn_species %in% c("Frozen milk", "NC")) 
+tab.milk <- subset(tab, Species %in% c( "milk_no_slits", "milk_slits")) 
 
-tab.iso <- subset(tab, Inoculumn_species %in% c("Leuconostoc lactis", "Leuconostoc mesenteroides", 
-                                                "Lactobacillus fermentum", "NC"))
+tab.iso <- subset(tab, Species %in% c("Leuconostoc lactis", "Leuconostoc mesenteroides", 
+                                                "Lactobacillus fermentum", "saline"))
 tab.LABr <- subset(tab.iso, Rifampicin %in% c("R", "NC"))
 tab.LAB <- subset(tab.iso, Rifampicin %in%  c("WT", "NC"))
 
 # Define function for plot log CFU count over time
-CFUoverT <- function(x, w, h, path.out) {
-  tabm <- melt(x, id.vars = c("SampleID", "Inoculum", "Inoculumn_species", 
-                              "Aging_Time", "Aging_Temperature", "Rifampicin"))
-  tabm$Aging_Time <- factor(tabm$Aging_Time, levels = c("Inoculation", "0D", "5D"))
-  tabm$Inoculumn_species <- factor(tabm$Inoculumn_species, levels = c("NC", 
-                                                                      "Leuconostoc lactis", "Leuconostoc mesenteroides", 
-                                                                      "Lactobacillus fermentum"))
+CFUoverT <- function(x, path.out, w, h) {
+  tabm <- melt(x, id.vars = c("SampleID", "Inoculum", "Species", 
+                              "Aging_Time", "Aging_Temperature", "Rifampicin", "pH"))
+  tabm$Aging_Time <- factor(tabm$Aging_Time, levels = c("Inoculation", "0D", "5D", "11D"))
+  tabm$Species <- factor(tabm$Species, levels = c("saline", "Leuconostoc lactis", 
+                                                  "Leuconostoc mesenteroides",
+                                                  "Lactobacillus fermentum", 
+                                                  "milk_no_slits", "milk_slits"))
   
-  #pdf(path.out, w, h)
-  ggplot(tabm, aes(x = Aging_Time, y = value, color = Inoculumn_species)) +
-    geom_boxplot() +
-    facet_wrap("variable") +
-    theme_bw()
-  #dev.off()
+  pdf(path.out, w, h)
+  print(ggplot(tabm, aes(x = Aging_Time, y = value, color = Species)) +
+          geom_boxplot() +
+          ylab("Log10(CFU/mL)") +
+          facet_wrap("variable") +
+          theme_bw(base_size = 16))
+  dev.off()
 }
 
+CFUoverT(tab.LAB, path.out = file.path(path, "30C_aging_isolates.pdf"), w = 10, h = 2.8)
+CFUoverT(tab.LABr, path.out = file.path(path, "30C_aging_isolatesR.pdf"), w = 10, h = 2.8)
+CFUoverT(tab.milk, path.out = file.path(path, "30C_aging_milk.pdf"), w = 10, h = 2.8)
 
-CFUoverT(tab.LABr)
+# KW for CFU count 
 
 
-
-
-
-
+# Slit area analysis
 tab.area <- read.csv(file.path(path,"image analysis.csv"))
 tab.area$Species <- factor(tab.area$Species, levels = c("saline", "Lactobacillus plantarum", 
                                           "milk_no_slits", "milk_slits",
